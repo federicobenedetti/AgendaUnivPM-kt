f9619dca85ca1406e9547d1276ef57d0f0969d90
Federico Benedetti <fede.benedetti9@gmail.com>
2021-07-04 11:49:00 +0200

    Tabs now are below, commonize card style

--- app/src/main/java/com/example/agendaunivpm/ui/main/PageViewModel.kt
+++ app/src/main/java/com/example/agendaunivpm/ui/main/PageViewModel.kt
@@ -10,10 +10,6 @@ class PageViewModel : ViewModel() {
 
     private val _index = MutableLiveData<Int>()
 
-    val text: LiveData<String> = Transformations.map(_index) {
-        "Hello world from section: $it"
-    }
-
     fun setIndex(index: Int) {
         _index.value = index
     }
