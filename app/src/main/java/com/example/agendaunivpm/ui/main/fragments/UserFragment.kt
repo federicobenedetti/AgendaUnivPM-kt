dc8d4d864495c1a1f3f366bc6730a8611635fc55
Federico Benedetti <fede.benedetti9@gmail.com>
2021-07-05 12:32:26 +0200

    Init user page

--- app/src/main/java/com/example/agendaunivpm/ui/main/fragments/UserFragment.kt
+++ app/src/main/java/com/example/agendaunivpm/ui/main/fragments/UserFragment.kt
@@ -7,15 +7,15 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.agendaunivpm.R
 
-class UserFragment: Fragment() {
+class UserFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_user, container, false)
     }
+
 
     companion object {
         @JvmStatic
