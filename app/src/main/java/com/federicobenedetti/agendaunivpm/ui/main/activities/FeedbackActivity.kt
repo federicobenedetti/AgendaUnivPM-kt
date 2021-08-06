b9a0be154a372e59b498451b545d70dc3ee96c52
Federico Benedetti <fede.benedetti9@gmail.com>
2021-08-07 01:37:29 +0200

    Send feedback now works!

--- app/src/main/java/com/federicobenedetti/agendaunivpm/ui/main/activities/FeedbackActivity.kt
+++ app/src/main/java/com/federicobenedetti/agendaunivpm/ui/main/activities/FeedbackActivity.kt
@@ -1,37 +1,82 @@
 package com.federicobenedetti.agendaunivpm.ui.main.activities
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.federicobenedetti.agendaunivpm.R
 import com.federicobenedetti.agendaunivpm.databinding.ActivityFeedbackBinding
+import com.federicobenedetti.agendaunivpm.ui.main.utils.CustomAppCompatActivity
 import com.federicobenedetti.agendaunivpm.ui.main.viewmodels.FeedbackViewModel
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.functions.FirebaseFunctions
+import com.google.firebase.functions.FirebaseFunctionsException
+import com.google.firebase.functions.ktx.functions
+import com.google.firebase.ktx.Firebase
 
-class FeedbackActivity : AppCompatActivity() {
+class FeedbackActivity : CustomAppCompatActivity("FEEDBACK") {
 
     private lateinit var feedbackBinding: ActivityFeedbackBinding
     private lateinit var feedbackViewModel: FeedbackViewModel
 
-    private var mBtnSendFeedback: Button? = null
+    private lateinit var mBtnSendFeedback: Button
+
+    private lateinit var functions: FirebaseFunctions
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_feedback)
 
         feedbackBinding = DataBindingUtil.setContentView(this, R.layout.activity_feedback)
-
         feedbackViewModel = ViewModelProvider(this).get(FeedbackViewModel::class.java)
         feedbackBinding.feedbackViewModel = feedbackViewModel
         feedbackBinding.lifecycleOwner = this
 
         mBtnSendFeedback = findViewById(R.id.btnSendFeedback)
-        mBtnSendFeedback!!.setOnClickListener {
-            // TODO: Send feedback here
-            Toast.makeText(this, R.string.feedback_sent, Toast.LENGTH_LONG).show();
-            finish()
+        mBtnSendFeedback.setOnClickListener {
+            onSendFeedbackClicked()
         }
+
+        functions = Firebase.functions
+    }
+
+    private fun sendUserFeedback(): Task<String> {
+        val data = hashMapOf(
+            "feedback" to feedbackViewModel.userFeedback.value
+        )
+
+        return functions
+            .getHttpsCallable("addUserFeedback")
+            .call(data)
+            .continueWith { task ->
+                val result = task.result?.data as? String
+                result
+            }
+    }
+
+    private fun onSendFeedbackClicked() {
+        Log.w(_logTAG, "User feedback: " + feedbackViewModel.userFeedback.value)
+        sendUserFeedback()
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    val e = task.exception
+                    if (e is FirebaseFunctionsException) {
+                        val code = e.code
+                        val details = e.details
+
+                        Log.w(_logTAG, "Failure code: $code")
+                        Log.w(_logTAG, "Failure details: $details")
+                    }
+
+                    Log.w(_logTAG, "addMessage:onFailure", e)
+                    return@OnCompleteListener
+                }
+
+                val result = task.result
+                Toast.makeText(this, R.string.feedback_sent, Toast.LENGTH_LONG).show();
+                finish()
+            })
     }
 }
--- app/src/main/java/com/federicobenedetti/agendaunivpm/ui/main/utils/FirebaseUtils.kt
+++ app/src/main/java/com/federicobenedetti/agendaunivpm/ui/main/utils/FirebaseUtils.kt
@@ -1,15 +1,24 @@
 package com.federicobenedetti.agendaunivpm.ui.main.utils
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.functions.FirebaseFunctions
+import com.google.firebase.functions.ktx.functions
+import com.google.firebase.ktx.Firebase
 
 object FirebaseUtils {
     private var mFirebaseAuth: FirebaseAuth? = null
+    private var mFirebaseFunctions: FirebaseFunctions? = null
 
     init {
         mFirebaseAuth = FirebaseAuth.getInstance()
+        mFirebaseFunctions = Firebase.functions
     }
 
     fun getFirebaseAuthInstance(): FirebaseAuth? {
         return mFirebaseAuth
+    }
+
+    fun getFirebaseFunctionsInstance(): FirebaseFunctions? {
+        return mFirebaseFunctions
     }
 }
