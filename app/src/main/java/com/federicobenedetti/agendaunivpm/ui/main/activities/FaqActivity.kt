ed7ddf9eda19ee1c9cc7772c5d84045ea31d5453
Federico Benedetti <fede.benedetti9@gmail.com>
2021-08-14 22:44:14 +0200

    Faq page DONE: Harder than i thought..

--- app/src/main/java/com/federicobenedetti/agendaunivpm/ui/main/activities/FaqActivity.kt
+++ app/src/main/java/com/federicobenedetti/agendaunivpm/ui/main/activities/FaqActivity.kt
@@ -1,12 +1,17 @@
 package com.federicobenedetti.agendaunivpm.ui.main.activities
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.federicobenedetti.agendaunivpm.R
+import com.federicobenedetti.agendaunivpm.databinding.ActivityFaqBinding
+import com.federicobenedetti.agendaunivpm.ui.main.utils.CustomAppCompatActivity
 
-class FaqActivity : AppCompatActivity() {
+class FaqActivity : CustomAppCompatActivity("FAQ") {
+    private lateinit var faqBinding: ActivityFaqBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_faq)
+        faqBinding = DataBindingUtil.setContentView(this, R.layout.activity_faq)
+
     }
 }
