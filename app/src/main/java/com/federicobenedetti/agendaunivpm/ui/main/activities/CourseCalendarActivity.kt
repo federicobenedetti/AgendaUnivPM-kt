b9551d419aee869d5df0aa21e45d6c97b4c9c797
Federico Benedetti <fede.benedetti9@gmail.com>
2021-08-17 15:53:19 +0200

    Prep VM / Activities

--- app/src/main/java/com/federicobenedetti/agendaunivpm/ui/main/activities/CourseCalendarActivity.kt
+++ app/src/main/java/com/federicobenedetti/agendaunivpm/ui/main/activities/CourseCalendarActivity.kt
@@ -1,6 +1,26 @@
 package com.federicobenedetti.agendaunivpm.ui.main.activities
 
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.federicobenedetti.agendaunivpm.R
+import com.federicobenedetti.agendaunivpm.databinding.ActivityCourseCalendarBinding
 import com.federicobenedetti.agendaunivpm.ui.main.utils.CustomAppCompatActivity
+import com.federicobenedetti.agendaunivpm.ui.main.viewmodels.CourseCalendarViewModel
 
 class CourseCalendarActivity : CustomAppCompatActivity("COURSECALENDAR") {
+
+    private lateinit var courseCalendarBinding: ActivityCourseCalendarBinding
+    private lateinit var courseCalendarViewModel: CourseCalendarViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        courseCalendarBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_course_calendar)
+        courseCalendarViewModel =
+            ViewModelProvider(this).get(CourseCalendarViewModel::class.java)
+        courseCalendarBinding.courseCalendarViewModel = courseCalendarViewModel
+        courseCalendarBinding.lifecycleOwner = this
+    }
 }
---
+++ app/src/main/java/com/federicobenedetti/agendaunivpm/ui/main/viewmodels/CourseCalendarViewModel.kt
@@ -0,0 +1,6 @@
+package com.federicobenedetti.agendaunivpm.ui.main.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class CourseCalendarViewModel : ViewModel() {
+}
